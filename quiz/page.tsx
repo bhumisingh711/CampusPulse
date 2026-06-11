0312c6c3167beef8031589f5a5fb53067ea9edac
bhumisingh711 <bhumivlogs79@gmail.com>
2026-06-11 19:53:38 +0530

    Create page.tsx

---
+++ quiz/page.tsx
@@ -0,0 +1,10 @@
+import ClubMatchmakerQuiz from '@/components/club-matchmaker-quiz';
+
+export const metadata = {
+  title: 'Club Matchmaker Quiz - CampusPulse',
+  description: 'Find your perfect club match with our intelligent quiz',
+};
+
+export default function QuizPage() {
+  return <ClubMatchmakerQuiz />;
+}
